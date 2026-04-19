a32c397bbd5e2e54f01166658bb45f524e0ae1f4
Mohammad Bilal Butt <mohammadbilal>
2026-04-19 13:20:15 +0200

    Zugegenerator liefert nun eine Dictionary, statt eine Liste, Eckfelder und Thron werden mit beachtet

--- Tablut/scr/board.py
+++ Tablut/scr/board.py
@@ -24,56 +24,74 @@ def check_valid_figure(White,figure):
 #Haupt-Funktion die dafür da ist alle Möglichen Spielzüge des jeweiligen Spielers zu bestimmen 
 def Zuggenerator(State,White):
     all_player_positions = get_all_positions(State,White)
-    all_moves = []
+    all_moves = {}
     
     for  (row,col) in all_player_positions:
-        all_moves.extend(get_figures_Moves(State,White,(row,col)))
+        all_moves = merge_moves(all_moves,(get_figures_Moves(State,White,(row,col))))
     
     return all_moves    
 
 # Gibt alle möglichen Zug der jeweiligen Figur (Position) an    
 def get_figures_Moves(State,White,Pos): 
-    moves = []
+    moves = {}
+    
     start_line,start_row = Pos 
      
-    moves.extend(all_moves_up_to_Figure(State,Pos))
+    moves = merge_moves(moves,(all_moves_up_to_Figure(State,Pos)))
+
+    moves= merge_moves(moves,(all_moves_down_to_Figure(State,Pos)))
     
-    moves.extend(all_moves_down_to_Figure(State,Pos))
+    moves= merge_moves(moves,(all_moves_left_to_Figure(State,Pos)))
+    
+    moves= merge_moves(moves,(all_moves_right_to_Figure(State,Pos)))
             
-    moves.extend(all_moves_left_to_Figure(State,Pos))
     
-    moves.extend(all_moves_right_to_Figure(State,Pos))
     
     return moves
 
-def all_moves_up_to_Figure(State, StartPos):
-    moves = []
-    (start_line,start_row) = StartPos
+def merge_moves(oldMoves :dict ,newMoves: dict):
+    
+    for key, values in newMoves.items():
+        if key in oldMoves:
+            oldMoves[key].extend(values)
+        else:
+            oldMoves[key] = values
+    
+    return oldMoves
+    
+
+def all_moves_up_to_Figure(board, StartPos):
+    moves = {}
+    (start_row,start_col) = StartPos
 
     row, col = StartPos
     row -= 1
 
     while row >= 0:
             
-        if isEmptyField(State,(row,col)):
-            moves.append(((start_line,start_row),(row, col)))
+        if isEmptyField(board,(row,col)):
+            if validMove(board,(row,col),StartPos):
+                moves = merge_moves(moves,{(start_row,start_col):[(row, col)]})
+                #moves.append(((start_row,start_col),(row, col)))
             row -=1
         else: 
             break
             
     return moves
 
-def all_moves_down_to_Figure(State, StartPos):
-    moves = []
-    (start_line,start_row) = StartPos
+def all_moves_down_to_Figure(board, StartPos):
+    moves = {}
+    (start_row,start_col) = StartPos
     
     row, col = StartPos
     row += 1
 
-    while row <= (len(State)-1):
+    while row <= (len(board)-1):
 
-        if isEmptyField(State,(row,col)):
-            moves.append(((start_line,start_row),(row, col)))
+        if isEmptyField(board,(row,col)):
+            if validMove(board,(row,col),StartPos):
+                moves = merge_moves(moves,{(start_row,start_col):[(row, col)]})
+                #moves.append(((start_line,start_row),(row, col)))
             row += 1
         else: 
 
@@ -82,33 +100,36 @@ def all_moves_down_to_Figure(State, StartPos):
     return moves
     
 
-def all_moves_left_to_Figure(State, StartPos):
-    moves = []
-    (start_line,start_row) = StartPos
+def all_moves_left_to_Figure(board, StartPos):
+    moves = {}
+    (start_row,start_col) = StartPos
     
     row, col = StartPos
     col -= 1
     while col >= 0:
         
-        if isEmptyField(State,(row,col)):
-            moves.append(((start_line,start_row),(row, col)))
+        if isEmptyField(board,(row,col)):
+            if validMove(board,(row,col),StartPos):
+                moves = merge_moves(moves,{(start_row,start_col):[(row, col)]})
+                #moves.append(((start_row,start_col),(row, col)))
             col -= 1                
         else: 
             break
     
     return moves
 
-def all_moves_right_to_Figure(State, StartPos):
+def all_moves_right_to_Figure(board, StartPos):
     
-    moves = []
-    (start_line,start_row) = StartPos
+    moves = {}
+    (start_row,start_col) = StartPos
     
     row, col = StartPos
     col += 1
 
-    while col <= (len(State[0])-1):
-        if isEmptyField(State,(row,col)):
-            moves.append(((start_line,start_row),(row, col)))
+    while col <= (len(board[0])-1):
+        if isEmptyField(board,(row,col)):
+            if validMove(board,(row,col),StartPos):
+                moves = merge_moves(moves,{(start_row,start_col):[(row, col)]})
             col += 1
         else: 
             break
@@ -121,6 +142,24 @@ def isEmptyField(State,Pos):
     row, col = Pos
     return State[row][col] == "E"
 
+#Funktion prüft, ob die die Zielposition ein valider Zug ist, ein Zug is INVALIDE wenn gilt:
+#Position Thronfeld ist
+#Ein Bauer (Schwarz oder Weiß) versucht eins der Eckfelder/Zielfelder betreten versucht
+def validMove(board,Pos,StartPos):
+    
+    start_row, start_col = StartPos
+    row,col = Pos
+    
+    #Thronfeld wird versucht zu betreten, illegaler Move
+    if (row,col) == (4,4):
+        return False
+        
+    #Bauer versucht Eckfeld/Zielfeld zu betreten, illegaler Move
+    if ((row,col) == (0,0) or (row,col) == ( 0,(len(board[0])-1) ) or (row,col) == (len(board)-1,0) or (row,col) == (len(board)-1,(len(board[8])-1)) ):
+        return (board[start_row][start_col] == "K")
+    
+
+    return True
 
 
 
@@ -176,17 +215,14 @@ def print_possible_Moves(list_Moves):
     for key, value in gruppen.items():
         print(f"{key} -> {value}")
             
-    
-        
-            
 
 TestBoard = [
-    ["W","E","B","E","E","E","E","E","E"],
     ["E","E","E","E","E","E","E","E","E"],
     ["E","E","E","E","E","E","E","E","E"],
-    ["B","E","E","E","B","E","E","E","E"],
-    ["E","E","E","E","K","B","E","E","E"],
-    ["E","E","E","E","B","E","E","E","E"],
+    ["E","E","E","E","E","E","E","E","E"],
+    ["E","E","E","E","E","E","E","E","E"],
+    ["B","E","E","E","E","E","E","E","E"],
+    ["E","E","E","E","E","E","E","E","E"],
     ["E","E","E","E","E","E","E","E","E"],
     ["E","E","E","E","E","E","E","E","E"],
     ["E","E","E","E","E","E","E","E","E"],
@@ -194,8 +230,13 @@ TestBoard = [
 
 #print_board(TestBoard)
 
-x = Zuggenerator(TestBoard,True)
-print(x)
+def print_dic(dict):
+    for key, value in dict.items():
+        print(f"{key} --> {value}")
 
-print("NUN SCHÖNERE VERSION:")
-print_possible_Moves(x)
+x = Zuggenerator(TestBoard,False)
+print_dic(x)
+
+
+#print("NUN Output, wenn man Dictionary als eine große Liste Nutzt:")
+#print_possible_Moves(x)
